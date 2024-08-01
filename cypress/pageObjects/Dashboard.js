8c89857735de5dec5b21f93f6bd2846dc2992db5
rudolfs-TDL <146933960+rudolfs-TDL@users.noreply.github.com>
2024-08-01 22:45:34 +0300

    Code review

--- cypress/pageObjects/Dashboard.js
+++ cypress/pageObjects/Dashboard.js
@@ -1,9 +1,9 @@
 class Dashboard {
   elements = {
     dashboardLink: () => cy.getByTestId("nav-dashboard-link"),
-    ordersLink: () => cy.get(':nth-child(4) > [data-testid="orders-link"]'),
+    ordersLink: () => cy.get(':nth-child(4) > [data-testid="orders-link"]'), // TODO: try to replace selector without :nth-child(4)
     firstOrderCard: () => cy.getByTestId("order-card").first(),
-    profileLink: () => cy.get(':nth-child(2) > [data-testid="profile-link"]'),
+    profileLink: () => cy.get(':nth-child(2) > [data-testid="profile-link"]'), // TODO: try to replace selector without :nth-child(2)
     
     phoneEdit: () => cy.getByTestId("account-phone-editor").find('[data-testid="edit-button"]'), 
     phoneInput: () => cy.getByTestId("phone-input"),
